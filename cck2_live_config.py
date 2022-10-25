218e976b064f9c211c3dff847f4aa2bd44b6e4d3
Thomas FORELL <thomas.forell@web.de>
2022-10-25 03:15:16 +0200

    put team data into box

--- cck2_live_config.py
+++ cck2_live_config.py
@@ -3,7 +3,7 @@ import os
 from pathlib import Path
 import sys
 
-from PySide2.QtWidgets import QApplication, QWidget, QGridLayout, QFormLayout, QSpinBox, QLineEdit, QCheckBox, QPushButton, QLabel
+from PySide2.QtWidgets import QApplication, QWidget, QGridLayout, QFormLayout, QSpinBox, QLineEdit, QCheckBox, QPushButton, QLabel, QGroupBox, QVBoxLayout
 from PySide2.QtCore import QFile, QSize
 from PySide2.QtGui import QIcon, QPixmap
 
@@ -14,21 +14,25 @@ class Widget(QWidget):
         self.load_ui()
 
     def load_ui(self):
-        grid = QGridLayout(self)
+        vbox = QGridLayout(self)
+
+        boxTeam = QGroupBox("Team")
+        grid = QGridLayout(boxTeam)
         grid.addWidget(QPushButton("<-"), 0, 0)
-        grid.addWidget(QLabel("Mannschaft"), 0, 1)
-        grid.addWidget(QPushButton("->"), 0, 2)
+        grid.addWidget(QPushButton("->"), 0, 1)
+        grid.addWidget(QPushButton("+"), 0, 2)
+        grid.addWidget(QPushButton("-"), 0, 3)
         form = QFormLayout()
 
-        iconHeim = QPixmap("../cck2_live/Logos/SKC_Nibelungen_Lorsch.png")
-        iconGast = QPixmap("../cck2_live/Logos/BW_Wiesbaden.jpg")
+        logoHeim = QPixmap("../cck2_live/Logos/SKC_Nibelungen_Lorsch.png")
+        logoGast = QPixmap("../cck2_live/Logos/BW_Wiesbaden.jpg")
         heim = QPushButton()
         gast = QPushButton()
 
-        heim.setIcon(iconHeim)
+        heim.setIcon(logoHeim)
         heim.setIconSize(QSize(100, 100))
         heim.setFixedSize(110, 110)
-        gast.setIcon(iconGast)
+        gast.setIcon(logoGast)
         gast.setIconSize(QSize(100, 100))
         gast.setFixedSize(110, 110)
 
@@ -44,9 +48,12 @@ class Widget(QWidget):
 
         form.addRow("Data File", QLineEdit("mannschaft1.json"))
 
-        grid.addLayout(form, 1, 0, 1, 3)
-        grid.addWidget(QPushButton("Werbung"), 2, 0, 1, 3)
-        grid.addWidget(QPushButton("Speichern"), 3, 0, 1, 3)
+        grid.addLayout(form, 1, 0, 1, 4)
+        boxTeam.setLayout(grid)
+
+        vbox.addWidget(boxTeam)
+        vbox.addWidget(QPushButton("Werbung"))
+        vbox.addWidget(QPushButton("Speichern"))
         self.setLayout(grid)
 
 if __name__ == "__main__":
